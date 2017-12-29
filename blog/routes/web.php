22159604c950dbb0a4375791be21a4bb16682433
VampiroJudo <johnconley507@hotmail.com>
2017-12-29 10:40:43 -0500

    making new migrations/setting up database tables (simple table for start of project) and making new views

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -12,10 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome') ->with('name', 'World');
 });
 
 
-Route::get('/', function () {
-	return view('about');
-});
