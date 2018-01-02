9529e6870015ee0a9ab017cf2082d6b3bf58b380
VampiroJudo <johnconley507@hotmail.com>
2018-01-02 15:47:51 -0500

    adding querying methodology to model

--- blog/app/Task.php
+++ blog/app/Task.php
@@ -5,8 +5,9 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model {
-
-	public function isComplete() {
-		return false;
-	}
+    
+    public  function scopeIncomplete() {
+    	return static::where('completed', 0)->get();
+    }
 }
+
