1ba3c635af8082b837165354fd22ce078a5af1e6
VampiroJudo <johnconley507@hotmail.com>
2018-01-02 15:21:49 -0500

    finishing creation of eloquent model

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,24 +1,16 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Task;
+
 
 Route::get('/tasks', function () {
-	$tasks = App\Tasks::all();
+	$tasks = Tasks::all();
 
 	return view('tasks.index', compact('tasks'));
 });
 
 Route::get('/tasks/{task}', function ($id) {
-	$tasks = DB::table('tasks')->finc($id);
+	$tasks = Task::find($id);
 
 	return view('tasks.show', compact('task'));
 });
