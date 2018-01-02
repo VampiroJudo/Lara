19033357e48c803b6e58f82f488245cbae7f46a7
VampiroJudo <johnconley507@hotmail.com>
2018-01-02 15:52:44 -0500

    fixing querying copde

--- blog/app/Task.php
+++ blog/app/Task.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model {
     
-    public  function scopeIncomplete() {
-    	return static::where('completed', 0)->get();
+    public function scopeIncomplete($query) {
+    	return $query->where('completed', 0);
     }
 }
 
