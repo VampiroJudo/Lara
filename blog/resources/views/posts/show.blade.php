9f41278bb0a6327eb008ac6623673facddfad55d
VampiroJudo <johnconley507@hotmail.com>
2018-01-06 10:43:40 -0500

    adding ability to show new posts

---
+++ blog/resources/views/posts/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts')
+
+@section('content')
+
+	<h1>A place to show the post.</h1>
+
+@endsection
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 
+Route::get('/posts/{post}', 'PostsController@show');
+
 Route::get('/', 'PostController@index');
 
 //controller => :Posts Controller
